8977a8a61f091e50153ad7fe3a99a3323d979785
ims <ims21@users.sourceforge.net>
2015-03-08 18:28:53 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setup_translate
 
 pkg = 'Extensions.ModifyPLiFullHD'
 setup (name = 'enigma2-plugin-extensions-modifyplifullhd',
-       version = '1.20',
+       version = '1.21',
        description = 'modify font and colors in pli-fullhd and pli-hd1 skins',
        packages = [pkg],
        package_dir = {pkg: 'plugin'},
