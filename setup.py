2ddfd1d76bc46928339191a133bffee84a5e33d5
ims <ims21@users.sourceforge.net>
2020-05-01 09:53:08 +0200

    Remove whitespaces

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setup_translate
 
 pkg = 'Extensions.ModifyPLiFullHD'
 setup (name = 'enigma2-plugin-extensions-modifyplifullhd',
-       version = '1.39',
-       description = 'modify font and colors for PLi-FullHD and PLi-HD1 skins',
-       packages = [pkg],
-       package_dir = {pkg: 'plugin'},
-       package_data = {pkg: ['png/*.png', 'locale/*.pot', 'locale/*/LC_MESSAGES/*.mo']},
-       cmdclass = setup_translate.cmdclass, # for translation
-      )
+	version = '1.39',
+	description = 'modify font and colors for PLi-FullHD and PLi-HD1 skins',
+	packages = [pkg],
+	package_dir = {pkg: 'plugin'},
+	package_data = {pkg: ['png/*.png', 'locale/*.pot', 'locale/*/LC_MESSAGES/*.mo']},
+	cmdclass = setup_translate.cmdclass, # for translation
+	)
